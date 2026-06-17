023a87af7cd09e3a92315b4372a5ae1e3fdd4ac1
SHREYA <shreyavaradaraj78@gmail.com>
2026-06-17 16:42:45 +0530

    Add DBConnection class for MySQL database access

---
+++ DBConnection.java
@@ -0,0 +1,12 @@
+package com.auth;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBConnection {
+	private static final String URL = "jdbc:mysql://localhost:3306/authdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "shera78";
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
